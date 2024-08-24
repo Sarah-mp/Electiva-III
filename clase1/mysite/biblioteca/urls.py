1204ceb7e21dd85a1a0a887c5e4e6417beda945e
Sarah-mp <126619525+Sarah-mp@users.noreply.github.com>
2024-08-24 01:59:19 +0000

    clase2 = new StringBuilder();

--- clase1/mysite/biblioteca/urls.py
+++ clase1/mysite/biblioteca/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('vender', views.vender, name='vender_libro'),
+    path('vender/', views.vender, name='vender'),
+    path('prestar_libro/', views.prestar_libro, name='prestar_libro'),
+    path('leer/<int:num1>/<int:num2>/', views.leer, name='leer'),  # ruta para leer libros con parámetro de página.
+    path('sumadora/', views.sumadora, name='sumadora'), 
+    path('suma/', views.suma, name='suma'),  # ruta para sumar dos números.  # ruta para sumar dos números.
 ]
+
 
--- clase1/mysite/biblioteca/views.py
+++ clase1/mysite/biblioteca/views.py
@@ -1,13 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 #vistas  basadas en
 
 def index(request):
     print ("hola mundo...")
-    return HttpResponse("<h1 style='color:blue;'>Hola mundo..</h1> <a href='vender/'>vender</a>")
+    return render(request, "index.html")
 
 def vender (request):
-    return HttpResponse("Estamos vendiendo libros...")
+    return render(request, "vender.html")
+
+def prestar_libro (request):
+    return render(request, "prestar_libro.html")
+
+def leer(request, num1, num2):
+    return HttpResponse(f"la suma es: {num1 + num2} ")
+
+def sumadora(request):
+    return render(request, "formulario_suma.html")
+
+def suma(request):
+    num1=int(request.POST.get("num1"))
+    num2=int(request.POST.get("num2"))
+    return HttpResponse(f"la suma es: {num1 + num2}")
