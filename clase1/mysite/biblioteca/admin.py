8445220f207a0226695416fe69441886db8f6689
Sarah-mp <126619525+Sarah-mp@users.noreply.github.com>
2024-08-31 01:47:02 +0000

    clase3_base_datos

--- clase1/mysite/biblioteca/admin.py
+++ clase1/mysite/biblioteca/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import*
 
 # Register your models here.
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ["id", "titulo", "autor", "no_paginas", "fecha_publicacion", "clasificacion"]
+    search_fields =["titulo", "autor"]
+    list_filter = ["clasificacion", "fecha_publicacion"]
+
+admin.site.register(Libro, LibroAdmin)
--- clase1/mysite/biblioteca/models.py
+++ clase1/mysite/biblioteca/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=254)
+    autor = models.CharField(max_length=100)
+    no_paginas = models.IntegerField()
+    fecha_publicacion = models.DateField(help_text= "Fecha Publicacion")
+    CLASIFICAC = (
+        (0, ""),
+        (1, "Romance"),
+        (2, "Aventura"),
+        (3, "Ficción"),
+    )
+    clasificacion = models.IntegerField(choices=CLASIFICAC, default=0 )
+
+    def __str__(self) -> str:
+        return f"Título:{self.titulo} - Autor: {self.autor}"
+    
